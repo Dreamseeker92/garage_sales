00084f5a650a4982bdd566a51c2a103214e026f3
Timur <timur@localhost.localdomain>
2020-11-08 07:10:58 +0300

    Add sales handlers and routes

--- cmd/sales-api/internal/handlers/product.go
+++ cmd/sales-api/internal/handlers/product.go
@@ -7,6 +7,7 @@ import (
 	"github.com/pkg/errors"
 	"gorm.io/gorm"
 	"net/http"
+	"strconv"
 )
 
 // Products defines all of the handlers related to products. It holds the
@@ -23,7 +24,7 @@ func NewProductsHandler(db *gorm.DB) *Products {
 // List gets all products from the service layer and encodes them for the
 // client response.
 func (p *Products) List(response http.ResponseWriter, request *http.Request) error {
-	list, err := product.List(p.DB)
+	list, err := product.List(request.Context(), p.DB)
 	if err != nil {
 		return errors.Wrap(err, "Listing products")
 	}
@@ -34,7 +35,7 @@ func (p *Products) List(response http.ResponseWriter, request *http.Request) err
 // Fetches a product by id.
 func (p *Products) Fetch(response http.ResponseWriter, request *http.Request) error {
 	id := chi.URLParam(request, "id")
-	product, err := product.Fetch(p.DB, id)
+	product, err := product.Fetch(request.Context(), p.DB, id)
 
 	if err != nil {
 		switch err {
@@ -57,10 +58,55 @@ func (p *Products) Create(response http.ResponseWriter, request *http.Request) e
 		return err
 	}
 
-	product, err := product.Persist(p.DB, &newProduct)
+	product, err := product.Persist(request.Context(), p.DB, &newProduct)
 	if err != nil {
 		return errors.Wrap(err, "Persisting a product")
 	}
 
 	return web.Respond(response, product, http.StatusOK)
+}
+
+func (p *Products) PersistSale(response http.ResponseWriter, request *http.Request) error {
+	var sale *product.Sale
+
+	if err := web.Decode(request, &sale); err != nil {
+		return errors.Wrap(err, "Decoding new sale")
+	}
+
+	err := product.PersistSale(request.Context(), p.DB, sale)
+	if err != nil {
+		return errors.Wrap(err, "Adding new sale")
+	}
+
+	return web.Respond(response, sale, http.StatusCreated)
+}
+
+func (p *Products) ListSales(response http.ResponseWriter, request *http.Request) error {
+	sales, err := product.ListSales(request.Context(), p.DB)
+	if err != nil {
+		return errors.Wrap(err, "Listing sales")
+	}
+
+	return web.Respond(response, sales, http.StatusOK)
+}
+
+func (p *Products) FetchSale(response http.ResponseWriter, request *http.Request) error {
+	id, err := strconv.Atoi(chi.URLParam(request, "id"))
+	if err != nil {
+		return errors.Wrap(err, "Parsing url")
+	}
+	sale, err := product.FetchSale(request.Context(), p.DB, id)
+
+	if err != nil {
+		switch err {
+		case gorm.ErrRecordNotFound:
+			return web.NewRequestError(err, http.StatusNotFound)
+		case gorm.ErrInvalidData:
+			return web.NewRequestError(err, http.StatusBadRequest)
+		default:
+			return errors.Wrapf(err, "getting sale %q", id)
+		}
+	}
+
+	return web.Respond(response, sale, http.StatusOK)
 }
--- cmd/sales-api/internal/handlers/routes.go
+++ cmd/sales-api/internal/handlers/routes.go
@@ -8,13 +8,19 @@ import (
 )
 
 // API constructs a handler which knows about all API routes.
-func API(db *gorm.DB, logger *log.Logger) http.Handler  {
+func API(db *gorm.DB, logger *log.Logger) http.Handler {
 	productsHandler := NewProductsHandler(db)
-	
+
 	app := web.NewApp(logger)
+	// Products
 	app.Handle(http.MethodGet, "/v1/products", productsHandler.List)
 	app.Handle(http.MethodGet, "/v1/products/{id}", productsHandler.Fetch)
 	app.Handle(http.MethodPost, "/v1/products", productsHandler.Create)
-	
+
+	// Sales
+	app.Handle(http.MethodGet, "/v1/sales", productsHandler.ListSales)
+	app.Handle(http.MethodGet, "/v1/sales/{id}", productsHandler.FetchSale)
+	app.Handle(http.MethodPost, "/v1/sales", productsHandler.PersistSale)
+
 	return app
 }
